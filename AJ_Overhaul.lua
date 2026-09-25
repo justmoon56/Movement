69e39bd2eef86bfd574f06d91dfff475c1c3903b
Moon <arfareza147@gmail.com>
2026-09-25 21:07:59 +0700

    Create AJ_Overhaul.lua

---
+++ AJ_Overhaul.lua
@@ -0,0 +1,3 @@
+local Valid = "https://gist.githubusercontent.com/justmoon56/3301c5abd50ff717b78289a1873e2800/raw/ca6c1bcb3d5704ad1128caa53c765144c567b3ff/AutoJump"
+
+loadstring(game:HttpGet(Valid))()
